1cd9a3a7ff5d96ed492e390899900074d6d3729e
JehLabel <jehlabel@gmail.com>
2016-09-28 17:06:59 +0700

    Initial commit

---
+++ app/src/main/java/handri/septyandri/mytoolbar/MainActivity.java
@@ -0,0 +1,29 @@
+package handri.septyandri.mytoolbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("My Toolbar");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
